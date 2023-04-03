3aa29098670634f852bf13ac6cf5b27cccad4458
sondos naalwi <123701958+SondosNaalwi@users.noreply.github.com>
2023-04-03 10:51:10 -0700

    Add files via upload

---
+++ sort-algorithms.js
@@ -0,0 +1,47 @@
+class sortAlgorithms {
+    constructor(time) {
+        this.list = document.querySelectorAll(".cell");
+        this.size = this.list.length;
+        this.time = time;
+        this.help = new Helper(this.time, this.list);
+    }
+
+    // BUBBLE SORT
+    BubbleSort = async () => {
+        for(let i = 0 ; i < this.size - 1 ; ++i) {
+            for(let j = 0 ; j < this.size - i - 1 ; ++j) {
+                await this.help.mark(j);
+                await this.help.mark(j+1);
+                if(await this.help.compare(j, j+1)) {
+                    await this.help.swap(j, j+1);
+                }
+                await this.help.unmark(j);
+                await this.help.unmark(j+1);
+            }
+            this.list[this.size - i - 1].setAttribute("class", "cell done");
+        }
+        this.list[0].setAttribute("class", "cell done");
+    }
+//Slection Sort
+    SelectionSort = async () => {
+        for(let i = 0 ; i < this.size ; ++i) {
+            let minIndex = i;
+            for(let j = i ; j < this.size ; ++j) {
+                await this.help.markSpl(minIndex);
+                await this.help.mark(j);
+                if(await this.help.compare(minIndex, j)) {
+                    await this.help.unmark(minIndex);
+                    minIndex = j;
+                }
+                await this.help.unmark(j);
+                await this.help.markSpl(minIndex);
+            }
+            await this.help.mark(minIndex);
+            await this.help.mark(i);
+            await this.help.pause();
+            await this.help.swap(minIndex, i);
+            await this.help.unmark(minIndex);
+            this.list[i].setAttribute("class", "cell done");
+        }
+    }
+}
